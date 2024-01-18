7acae8e96f295578a71e17a4b5b4b8b06d17e3e5
colkito <github@colkito.com>
2024-01-17 21:49:14 -0300

    update test content

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -91,6 +91,7 @@ export const Blog = defineDocumentType(() => ({
     layout: { type: 'string' },
     bibliography: { type: 'string' },
     canonicalUrl: { type: 'string' },
+    aiContent: { type: 'boolean', default: false}
   },
   computedFields: {
     ...computedFields,
