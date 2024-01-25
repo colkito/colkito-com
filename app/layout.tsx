4c8693d7f9d4c352088bf57da013f3469c23a468
colkito <github@colkito.com>
2024-01-24 23:01:33 -0300

    update base font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,21 @@
 import 'css/tailwind.css'
 import 'pliny/search/algolia.css'
 
-import { Space_Grotesk } from 'next/font/google'
+import { Metadata } from 'next'
+import { Lato } from 'next/font/google'
 import { Analytics, AnalyticsConfig } from 'pliny/analytics'
 import { SearchProvider, SearchConfig } from 'pliny/search'
 import Header from '@/components/Header'
-import SectionContainer from '@/components/SectionContainer'
 import Footer from '@/components/Footer'
+import SectionContainer from '@/components/SectionContainer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
-import { Metadata } from 'next'
 
-const space_grotesk = Space_Grotesk({
+// Font config
+const lato = Lato({
+  weight: '400',
   subsets: ['latin'],
   display: 'swap',
-  variable: '--font-space-grotesk',
 })
 
 export const metadata: Metadata = {
@@ -61,7 +62,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html
       lang={siteMetadata.language}
-      className={`${space_grotesk.variable} scroll-smooth`}
+      className={`${lato.className} scroll-smooth`}
       suppressHydrationWarning
     >
       <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
