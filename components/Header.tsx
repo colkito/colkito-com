f888b46ca3237f183887d6d713936d2d39a3c369
colkito <github@colkito.com>
2024-01-26 13:48:09 -0300

    update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,10 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-// import Logo from '@/data/logo.svg'
+import Image from './Image'
 import Link from './Link'
 import MobileNav from './MobileNav'
-import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
+import ThemeSwitch from './ThemeSwitch'
 
 const Header = () => {
   return (
@@ -12,14 +12,18 @@ const Header = () => {
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
-            {/* <div className="mr-3">
-              <Logo />
-            </div> */}
-            {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="h-6 text-2xl font-semibold">{siteMetadata.headerTitle}</div>
-            ) : (
-              siteMetadata.headerTitle
-            )}
+            <div className="mr-3 flex">
+              <Image
+                alt="colkito"
+                src="/static/images/authors/default.jpg"
+                className="flex rounded-lg"
+                width={30}
+                height={30}
+              />
+            </div>
+            <div className="flex h-6 items-center text-2xl font-semibold">
+              {siteMetadata.headerTitle}
+            </div>
           </div>
         </Link>
       </div>
