74e99738ece61ccc31aa54adf0fe64bd0b7e2cf9
colkito <github@colkito.com>
2024-01-26 22:00:37 -0300

    update dark style

--- components/PostAIContent.tsx
+++ components/PostAIContent.tsx
@@ -2,7 +2,7 @@ import { SiOpenai } from 'react-icons/si'
 
 export default function PostAiContent() {
   return (
-    <div className="ml-2 flex items-center justify-center rounded-full border border-gray-300 bg-gray-100 px-2 text-xs uppercase text-gray-500">
+    <div className="ml-2 flex items-center justify-center rounded-full border border-gray-300 bg-gray-100 px-2 text-xs uppercase text-gray-500 dark:border-gray-700 dark:bg-gray-500 dark:text-gray-300">
       <SiOpenai className="mr-1" /> AI assisted content
     </div>
   )
