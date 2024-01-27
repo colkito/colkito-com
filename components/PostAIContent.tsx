6ceebda8f560b16e1095a007709058fc059d619c
colkito <github@colkito.com>
2024-01-26 22:22:11 -0300

    update dark mode style

--- components/PostAIContent.tsx
+++ components/PostAIContent.tsx
@@ -1,9 +1,9 @@
-import { SiOpenai } from 'react-icons/si'
+import { TbBrandOpenai } from 'react-icons/tb'
 
 export default function PostAiContent() {
   return (
-    <div className="ml-2 flex items-center justify-center rounded-full border border-gray-300 bg-gray-100 px-2 text-xs uppercase text-gray-500 dark:border-gray-700 dark:bg-gray-500 dark:text-gray-300">
-      <SiOpenai className="mr-1" /> AI assisted content
+    <div className="ml-2 flex items-center justify-center rounded-full border border-gray-300 bg-gray-100 px-2 text-xs uppercase text-gray-500 dark:border-slate-600 dark:bg-slate-800 dark:text-slate-400">
+      <TbBrandOpenai className="mr-1" /> AI assisted content
     </div>
   )
 }
